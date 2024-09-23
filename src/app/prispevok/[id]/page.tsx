7f333b409a78b28e3c9f2a62f52df1b25ac18950
dragox134 <legi1810@gmail.com>
2024-09-23 11:53:51 +0200

    added prispevok to structure(forgot to add it first time)

---
+++ src/app/prispevok/[id]/page.tsx
@@ -0,0 +1,16 @@
+
+// src/app/prispevok/[id]/page.tsx
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export const metadata = { title: 'Post Detail | ZoškaSnap'};
+
+export default function PostDetail() {
+
+  return (
+    <Container>   
+        <Typography> Post Detail </Typography>
+    </Container>
+  );
+}
---
+++ src/app/prispevok/page.tsx
@@ -0,0 +1,16 @@
+
+// src/app/prispevok/page.tsx
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export const metadata = { title: 'Listing Postov | ZoškaSnap'};
+
+export default function PostListing() {
+
+  return (
+    <Container>   
+        <Typography> Post Listing </Typography>
+    </Container>
+  );
+}
