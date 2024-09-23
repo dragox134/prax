3684b2ea7e02697145cc04a6b2bd91d08fbea081
dragox134 <legi1810@gmail.com>
2024-09-23 10:33:52 +0200

    first

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,14 @@
+
+// src/app/page.tsx
+
+import Typography from "@mui/material/Typography";
+
+export const metadata = { title: 'Domov | ZoškaSnap'};
+
+export default function Home() {
+
+  return (
+
+    <Typography> Domovská stránka </Typography>
+  );
+}
---
+++ src/app/o-mne/page.tsx
@@ -0,0 +1,16 @@
+
+// src/app/o-mne/page.tsx
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export const metadata = { title: 'Domov | ZoškaSnap'};
+
+export default function About() {
+
+  return (
+    <Container>
+        <Typography> Stránka o mne </Typography>
+    </Container>
+  );
+}
